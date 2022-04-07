e7a4a9464920d5e6e4aaddbcea2eed8d94a7c951
Saniabuh <Saniabuh@gmail.com>
2022-04-07 08:32:34 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Return: Always 0 (Sucess)
+ * Return: 0 (Sucess)
  */
 int main(void)
 {
-	puts(*\*Programming is like building a multilingual puzzle*);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
