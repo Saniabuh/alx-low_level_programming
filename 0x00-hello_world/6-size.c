3595a728afea56307aedcc3c91656ad95fb11872
Saniabuh <Saniabuh@gmail.com>
2022-04-07 14:25:42 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,6 @@
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
 printf("size of a char: %d byte(s)\n",(unsigned long)sizeof(d));
 printf("size of an int: %d byte(s)\n",(unsigned long)sizeo(a));
 printf("size of a long int: %d byte(s)\n",(unsigned long)sizeof(b));
