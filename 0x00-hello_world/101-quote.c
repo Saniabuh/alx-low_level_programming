bca9feddadba52eeb64bfc9b4a91b4b74ec51346
Saniabuh <Saniabuh@gmail.com>
2022-04-07 14:54:26 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,17 @@
 #include <unistd.h>
 
 /**
-* main - Entry point
-*
-* Return: 0 (Success)
+ * main - Entry point
+ *
+ * Return: 0 (Success)
 */
 
-int main (void)
+int main(void)
 
 {
-write(1,"and that piece of art is useful" -Dora Korpar,2015-10-19\n",59);
-return (1);
+
+  write(1,"and that piece of art is useful\" -Dora Korpar,2015-10-19\n",59);
+  return (1);
+
 }
+
