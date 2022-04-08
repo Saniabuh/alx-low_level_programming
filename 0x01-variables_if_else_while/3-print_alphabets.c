ec67da2401b3a46803d96c36a81cec9142bb3b04
Saniabuh <Saniabuh@gmail.com>
2022-04-09 00:51:49 +0100

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  * main - Entry point
  *
@@ -7,20 +9,20 @@
 int main(void)
 
 {
-	char lower_case;
-	char upper_case;
-	for (lower_case = 'a'; lower_case <= 'z'; lower_case++);
-	{
-		putchar (lower_case);
-	}
+char lower_case;
+char upper_case;
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+{
+putchar (lower_case);
+}
 
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++);
-	{
-		putchar (upper_case);
-	}
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+putchar(upper_case);	
+}
 
-	putchar ('\n');
+putchar('\n');
 
-	return (0);
+return (0);
 
 }
