6346f3c0b682cc8b26a334fabcf85ec20356a929
Saniabuh <Saniabuh@gmail.com>
2022-04-08 01:18:40 +0100

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,30 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
-<<<<<<< HEAD
-main - Function begins
-Return: Function ends
-=======
- * main - Entry point
+ * main	- Entry point
  *
- * Return: 0 (Success)
->>>>>>> 75a77658d3da27ffad53952bcd6bb642ac42efb7
-*/
-
+ * Return: 1
+ */
 int main(void)
-
 {
-
-<<<<<<< HEAD
-write(2,"and that piece of art is useful/" -Dora Korpar,2015-10-19\n",59);
-return (1)
-
+	write(1, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59
+	
+	return (1);
 }
-=======
-write(1,"and that piece of art is useful\" -Dora Korpar,2015-10-19\n",59);
-return (1);
-
-}
-
->>>>>>> 75a77658d3da27ffad53952bcd6bb642ac42efb7
