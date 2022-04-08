23b2c71cb342a60c517b675fd7473b4eb0a7a7bb
Saniabuh <Saniabuh@gmail.com>
2022-04-08 22:20:53 +0100

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry start
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+	
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+	{
+		if (alphabet == 'Q')
+			continue:
+		else if (alphabet = 'e')
+			continue:
+				putchar (alphabet);
+	}
+	putchar ('\n');
+
+	return (0);
+
+}
