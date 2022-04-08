16fcd16f5726c1d6e6830f0027cec2f5329c5a84
Saniabuh <Saniabuh@gmail.com>
2022-04-08 22:00:30 +0100

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+
+{
+	char alphabet;
+	for (alphabet = 'a', alphabet <= 'z', alphabet ++);
+{
+	putchar (alphabet);
+}
+putchar ('\n');
+
+return (0);
+
+}
