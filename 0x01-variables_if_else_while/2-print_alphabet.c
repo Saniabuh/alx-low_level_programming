a47272acdea77328962a1223b3f76f36c77443ea
Saniabuh <Saniabuh@gmail.com>
2022-04-09 00:46:05 +0100

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,11 @@ int main(void)
 
 {
 char alphabet;
-for (alphabet = 'a', alphabet <= 'z', alphabet ++);
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 }
+
 putchar('\n');
 
 return (0);
