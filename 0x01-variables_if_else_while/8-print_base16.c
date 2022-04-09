04287160ea9796da9432d83386e95e77b22e91a7
Saniabuh <Saniabuh@gmail.com>
2022-04-09 05:47:16 +0100

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,21 +6,17 @@
  * Return: Always 0 (correct)
  */
 int main(void)
-
-int base_16;
-for (base_16 = 48; base_16 <= 57; base_16++)
-
 {
-putchar(base_16);
+	char base_16;
+
+	for (base_16 = 48; base_16 <= 57; base_16++)
+	{
+		putchar(base_16);
+	}
+	for (bas2_16 = 97; base_16 <= 102; base_16++)
+	{
+		putchar(base_16);
+	}
+	putchar('\n');
+	return (0);
 }
-for (base_16 = 97; base_16 <= 102; base_16++)
-{
-putchar(base_16)
-}
-
-putchar('\n');
-
-return (0);
-
-}
-
