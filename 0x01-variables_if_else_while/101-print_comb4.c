21f54f16d743da280e09e4203753a2ee661ae0bd
Saniabuh <Saniabuh@gmail.com>
2022-04-09 18:47:03 +0100

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,10 +17,10 @@ int main(void)
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
-				if (digit1 == 7 && digit2 == 8 digit3 == 9)
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
 				putchar(',');
-				putchae(' ');
+				putchar(' ');
 			}
 		}
 	}
