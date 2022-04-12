3b5976e1dbb47bd4c73c8facce935bed197aaecf
Saniabuh <Saniabuh@gmail.com>
2022-04-12 12:20:08 +0100

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	-putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	-putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My first readme in 0x02-functions_nested_loops task
