db43ef604ac86c50e8f086963898690450e2b852
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-04-12 13:47:08 +0100

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * add - prints add two number
- *@i: print int n
- *@k: print in k
- * Return: Always 0.
- */
+* add - prints add two number
+*@i: print int n
+*@k: print in k
+* Return: Always 0.
+*/
 int add(int i, int k)
 {
-  return (i+k);
+return (i + k);
 }
  
