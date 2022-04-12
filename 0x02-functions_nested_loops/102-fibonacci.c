a25339bebc6717872ab321d747fd87bd5ebdddcc
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-04-12 15:41:14 +0100

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
-*main - Prints first 50 Fibonacci numbers, starting with 1 and 2,    
+* main - Prints first 50 Fibonacci numbers, starting with 1 and 2,    
 *
-*Return: Always 0.
+* Return: Always 0.
 */
 int main(void)
 {
