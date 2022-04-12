8f8907dc39313839fbfb85ef1615922bc3fbdb0c
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-04-12 14:45:04 +0100

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_times_table - Prints the times table of the input,
+ * starting with 0.
+ * @n: The value of the times table to be printed.
+ */
+void print_times_table(int n)
+{
+int num, mult, result;
+
+if (n >= 0 && n <= 15)
+{
+for (num = 0; num <= n; num++)
+{
+_putchar('0');
+
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
+
+result = num * mult;
+
+if (result <= 99)
+_putchar(' ');
+if (result <= 9)
+_putchar(' ');
+
+if (result >= 100)
+{
+_putchar((result / 100) + '0');
+_putchar(((result / 10)) % 10 + '0');
+}
+else if (result <= 99 && prod >= 10)
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
+}
+}
