87642dc5f0a6cf6d79d6791fb11b5d627df0c635
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-04-12 22:32:26 +0100

    Task 12 unique

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,56 +1,50 @@
-#include "main.h"
-/**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- * Return: void
- */
+/* betty*/
 
+#include "main.h"
+
+/**
+ *  * print_times_table- prints the n times table, starting with 0.
+ *   * @n : int n is the number of times table
+ *    * Return: Always 0.
+*/
 void print_times_table(int n)
 {
-	int a = 0, rep, b;
+	int i, j, k;
 
-	if (n < 0 || n > 15)
-		return;
-	while (a <= n)
+	if (n >= 0 && n <= 15)
 	{
-		for (b = 0; b <= n; b++)
+		for (i = 0; i <= n; i++)
 		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
+			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
 				if (j == 0)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + rep);
-				} 
-				else if (rep < 100)
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
+					_putchar(',');
 					_putchar(' ');
-					_putchar('0' + rep / 10);
-					_putchar(('0' + rep % 10);
-				} 
-				else
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
-					_putchar('0' + rep / 100);
-					_putchar('0' + (rep - 100) / 10);
-					_putchar('0' + rep % 10);
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}  else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
-						 if (b > n)
-						 {
-							 _putchar(',');
-                                                         _putchar(' ');
-						 }
-						 }
-						 _putchar('\n');
-						 a++;
-						 }
-						 }
-						 
-			
-		
-	
-
+			}
+			_putchar('\n');
+		}
+	}
+}
