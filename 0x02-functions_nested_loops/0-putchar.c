2edfcaafabf2b5d8f418152e59d74de010d3dcbc
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-04-12 12:58:01 +0100

    My task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,11 @@ int main(void)
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-	-putchar('t');
+	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	-putchar('r');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
