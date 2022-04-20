fbc5d8982713c7825b1722cc953dbfdf0d3abad2
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-04-20 09:27:09 +0100

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+* Return: return 0
+*/
+int main(void)
+ {
+	 int contador;
+	 int  checksum;
+	 int  aleatorio;
+
+	srand(time(NULL));
+	checksum = 2772;
+	contador = 0;
+
+	while (checksum > 122)
+	{
+		aleatorio = (rand() % 100);
+		printf("%c", aleatorio);
+		checksum -= aleatorio;
+		contador++;
+	}
+	printf("%c", checksum);
+        return 0;
+ }
