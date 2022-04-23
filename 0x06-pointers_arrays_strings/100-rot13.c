5819cb9d335ea3a877539cf8c7be2bc37f27b375
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-04-23 11:33:45 +0100

    Task 10

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,16 +9,16 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char *n1 = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
-	char *n2 = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
+	char *letters1 = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
+	char *letters2 = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == n1[j])
+			if (s[i] == letters1[j])
 			{
-				s[i] = n2[j];
+				s[i] = letters2[j];
 				break;
 			}
 		}
