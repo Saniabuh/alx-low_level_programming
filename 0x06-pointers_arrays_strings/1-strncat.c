df70a71d8021ce46c156283bafb70c75c372f42e
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-04-23 11:25:33 +0100

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,25 +7,25 @@
  *
  * Return: dest
  */
- char *_strncat(char *dest, char *src, int n);
- }
- int a;
- int b;
- 
- a = 0;
- 
- while(dest[a] != 0)
- {
- a++;
- }
- 
- b = 0;
- 
- while(src[b] != 0 && b < n)
- {
- dest[a] = src[b];
- a++;
- b++;
- }
- return (dest);
- }
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (dest[a] != 0)
+	{
+		a++;
+	}
+
+	b = 0;
+
+	while (src[b] != 0 && b < n)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
+}
