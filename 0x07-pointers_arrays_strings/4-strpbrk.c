5f005e08c4766ab7761a84f1ea87551fb0425e15
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-04-26 12:16:32 +0100

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+ * Return: Nothing.
+ 
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; *(accept + j); j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (0);
+}
