76ec08b099a67fe43de0305a67b7e7fa4804bcc6
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-02 21:04:27 +0100

    Task

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - prints 1 or 0 depending on input
+ *
+ * @c: first parameter
+ *
+ * Description: prints all lowercase letters
+ *
+ * REturn: Always(0)
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
