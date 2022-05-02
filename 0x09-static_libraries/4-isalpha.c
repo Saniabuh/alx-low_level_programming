6b86e5b3068453df7c449c120d68fdff3ad44e0d
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-02 21:09:35 +0100

    Task

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checl if character is an alphabet ccharacter
+ *
+ * @c: type int character
+ *
+ * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c < 'Z'))
+		return (1);
+
+	else
+		return (0);
+}
