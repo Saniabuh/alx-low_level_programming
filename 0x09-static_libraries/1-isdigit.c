4e7f586a09cee532ef7483fe26b04fe1a5dbd8a6
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-02 20:54:45 +0100

    Task

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ *
+ * @c: int to be checked
+ *
+ * Return: 1 if c is a digit, 0 if otherwise
+ */
+
+int _isdigt(int c)
+{
+	return (c >= '0' && c <= '9');
+}
