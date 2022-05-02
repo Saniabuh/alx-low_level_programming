ee2459d402c1d7e86e0223488463bbcbe185ffee
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-02 22:57:59 +0100

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,39 +1,25 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
- * _strchr - locates char in string
- * @s: pointer to string
- * @c: char to locate
+ * _strspn - prints buffer in hexa
+ * @s: buffer
+ * @accept: buffer2
  *
- * Return: pointer to first occurence of c in s
- */
-char *_strchr(char *s, char c)
-{
-	while (*s)
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
-	return (NULL);
-}
-
-/**
- * _strspn - gets length of prefix substring
- * @s: string to check
- * @accept: bytes composing prefix
- *
- * Return: integer length of substring
+ * Return: Nothing.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
+	unsigned int j, i;
 
-	while (*s && _strchr(accept, *s))
+	for (j = 0; s[j] != '\0'; j++)
 	{
-		s++;
-		i++;
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (s[j] == accept[i])
+				break;
+		}
+		if (!(accept[i]))
+			break;
 	}
-	return (i);
+	return (j);
 }
