32ff47f00e98651d1b61df1acf231fe89f362b11
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-02 23:25:33 +0100

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,24 +1,15 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - program that prints its name
- * @argc: argument count
- * @argv: argunment vector
+ * main - prints its name, followed by a new line
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arguments
  *
- * Return: int
+ * Return: 0 (Always)
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	int count;
-
-	for (count = 0; count < argc; count++)
-	{
-		if (argc == 1)
-			printf("%s\n", argv[count]);
-		else
-			break;
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }
