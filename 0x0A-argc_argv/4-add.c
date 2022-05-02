1b27e4c3777b6e5bc35330d39d17e679f7434670
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-02 23:33:45 +0100

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,55 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 
 /**
- * check_digit - checks if a given char is number or not
- * @a: input char
- * Return: int
- **/
-
-int check_digit(char *a)
-{
-	int i, num, len;
-
-	i = 0;
-	num = 0;
-	len = strlen(a);
-	while (i < len)
-	{
-		if (a[i] < '0' || a[i] > '9')
-		{
-			return (-1);
-		}
-		else
-			num = num * 10 + (a[i] - '0');
-		i++;
-	}
-	return (num);
-}
-
-/**
- * main -  program that adds positive numbers
- * @argc: arguement count
- * @argv: argument vector
- * Return: int
- **/
+ * main - adds positive numbers
+ *
+ * @argc: number of command line arguments
+ * @argv: array that contains the program command line arguments
+ *
+ * Return: 0 (Success)
+ */
 
 int main(int argc, char *argv[])
 {
-	int i, num, res;
+	int i, j, add = 0;
 
-	res = 0;
 	for (i = 1; i < argc; i++)
 	{
-		num = check_digit(argv[i]);
-		if (num == -1)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		res += num;
+		add += atoi(argv[i]);
 	}
-	printf("%d\n", res);
+	printf("%d\n", add);
 	return (0);
 }
