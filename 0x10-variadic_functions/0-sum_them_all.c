2ac02c00f9e8ce98c985de6a9a8a57ce4f139ec6
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-16 12:19:08 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - calculates the sum of all its parameters
@@ -19,5 +20,5 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_end(list);
 
-	return (sum);
+	return (0);
 }
