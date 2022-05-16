0c1129831e55c5f21c8c0dbf17922dd84982f639
Saniabuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-16 12:25:13 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,14 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 
-/**
- * sum_them_all -  sum all arguments
- * @n: int, number of undefined arguments
- *
- * Return: sum of args
- * On error, 0 is returned
- */
-
 int sum_them_all(const unsigned int n, ...)
 {
 va_list list;
