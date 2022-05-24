6b229f01f1cafec612fb12dccf6b3ff2ff61c718
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-24 12:59:18 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * add_node - adds a fresh node at beginning of a list
+ * add_node - adds a new node at beginning of a list
  * @head: pointer to the node
  * @str: value of the node
  *
@@ -11,20 +11,20 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *fresh;
+	list_t *new;
 	unsigned int len = 0;
 
 	while (str[len])
 		len++;
 
 	new = malloc(sizeof(list_t));
-	if (!fresh)
+	if (!new)
 		return (NULL);
 
-	fresh->len = len;
-	fresh->str = strdup(str);
-	fresh->next = (*head);
-	(*head) = fresh;
+	new->len = len;
+	new->str = strdup(str);
+	new->next = (*head);
+	(*head) = new;
 
 	return (*head);
 }
