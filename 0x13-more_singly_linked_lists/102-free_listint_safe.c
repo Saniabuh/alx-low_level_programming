3486099124a7240dbc1d28a4731e4345d0b59864
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-26 22:10:29 +0200

    Task

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,42 +7,22 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
-	listint_t *curr;
+        size_t counter = 0;
+        listint_t *safe_node = *h;
+        listint_t *tmp_node;
 
-	hptr = NULL;
-	while (*h != NULL)
-	{
-		new = malloc(sizeof(listp_t));
+        if (!h)
+                return (0);
+        while (safe_node != NULL)
+        {
+                counter++;
+                tmp_node = safe_node;
+                safe_node = safe_node->next;
+                free(tmp_node);
 
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)*h;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-
-		while (add->next != NULL)
-		{
-			add = add->next;
-			if (*h == add->p)
-			{
-				*h = NULL;
-				free_listp2(&hptr);
-				return (nnodes);
-			}
-		}
-
-		curr = *h;
-		*h = (*h)->next;
-		free(curr);
-		nnodes++;
-	}
-
-	*h = NULL;
-	free_listp2(&hptr);
-	return (nnodes);
+                if (tmp_node < safe_node)
+                        break;
+        }
+        *h = NULL;
+        return (counter);
 }
