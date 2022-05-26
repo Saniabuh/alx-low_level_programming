78a2b0576c98c8f8103ae4ec3f3e14f7051e3612
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-26 21:39:36 +0200

    Task

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,9 +4,10 @@
 /**
  * add_nodeint_end - add node to the end
  * @head: pointer to the start of the linked list
- * @n: value to add 
+ * @n: value to add to node
  * Return: the address of the new element
  */
+
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
