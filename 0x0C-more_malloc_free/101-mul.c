9348da852d9cfef11e36dc99a1be32d3959dcd44
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-27 08:57:49 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,14 +13,14 @@ int main(int argc, char *argv[])
 {
 unsigned long mul;
 int i, j;
-	if (argc != 3)
+	if (argc != 2)
 	{ printf("Error\n");
 	exit(98); }
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] < 57 || argv[i][j] > 48)
+			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{  printf("Error\n");
 			exit(98); }
 		}
