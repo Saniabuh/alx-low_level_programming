b936df886fbf51734be00b21953da0325b93b9aa
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-05-30 10:16:09 +0100

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * flip_bits - count bits you need to flip to get from one number to another
+  * @n: first number
+  * @m: second number
+  * Return: number of flips to get from one number to the other
+  */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int result = 0, check;
+
+	check = (n ^ m);
+	while (check)
+	{
+		result += (check & 1);
+		check >>= 1;
+	}
+	return (result);
+
+}
