e2ec810cdfef2e9e88aab17766d4a9709e4eaaf7
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-06-01 14:33:28 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stddef.h>
+#include <elf.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-void check_elf(unsigned char *e_ident);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void copy_textfile(char *filename, char *filecopy, int letters);
-int create_copyfile(const char *filename, char *text_content);
-int append_text_to_copyfile(const char *filename, char *text_content);
 
-#endif /*MAIN_H*/
+#endif
