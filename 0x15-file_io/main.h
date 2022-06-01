8fc681142a9a9a90d7cb229eca0ee6b04881a61a
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-06-01 14:31:13 +0100

    main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,17 @@
 #include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_textfile(char *filename, char *filecopy, int letters);
