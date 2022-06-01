390ca6d47913ab9df93e258f1217c62134ae4a29
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-06-01 10:39:25 +0100

    main

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+void copy_textfile(char *filename, char *filecopy, int letters);
+int create_copyfile(const char *filename, char *text_content);
+int append_text_to_copyfile(const char *filename, char *text_content);
+
+#endif /*MAIN_H*/
