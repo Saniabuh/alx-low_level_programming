1501755c230fcae26f4e21a70adb0f437e3f6259
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-07-08 00:51:18 +0200

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,11 +10,11 @@ size_t dlistint_len(const dlistint_t *h)
 {
 		size_t nodes = 0;
 
-			while (h)
-					{
-								nodes++;
-										h = h->next;
-											}
+		while (h)
+{
+		nodes++;
+		h = h->next;
+}
 
-				return (nodes);
+		return (nodes);
 }
