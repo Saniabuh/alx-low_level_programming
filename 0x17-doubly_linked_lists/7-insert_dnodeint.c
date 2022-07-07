9dc4e6a2608cfc6a123c06ab789a196db6e4f52a
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-07-08 01:04:15 +0200

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,39 @@
 #include "lists.h"
 
 /**
- *  * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
- *   *                            list at a given position.
- *    * @h: A pointer to the head of the dlistint_t list.
- *     * @idx: The position to insert the new node.
- *      * @n: The integer for the new node to contain.
- *       *
- *        * Return: If the function fails - NULL.
- *         *         Otherwise - the address of the new node.
- *          */
+ * insert_dnodeint_at_index - return the nth node of a dlistint list
+ * @h: type pointer dlistint nodes
+ * @idx: type unsigned int index of node
+ * @n: type int value of node
+ * Return: return the new node or NULL if failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-		dlistint_t *tmp = *h, *new;
+	dlistint_t *temp_node = *h;
+	dlistint_t *new_node;
 
-			if (idx == 0)
-						return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-				for (; idx != 1; idx--)
-						{
-									tmp = tmp->next;
-											if (tmp == NULL)
-															return (NULL);
-												}
+	for (; idx != 1; idx--)
+	{
+		temp_node = temp_node->next;
+		if (temp_node == NULL)
+			return (NULL);
 
-					if (tmp->next == NULL)
-								return (add_dnodeint_end(h, n));
+	}
+	if (temp_node->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-						new = malloc(sizeof(dlistint_t));
-							if (new == NULL)
-										return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-								new->n = n;
-									new->prev = tmp;
-										new->next = tmp->next;
-											tmp->next->prev = new;
-												tmp->next = new;
+	new_node->n = n;
+	new_node->prev = temp_node;
+	new_node->next = temp_node->next;
+	temp_node->next->prev = new_node;
+	temp_node->next = new_node;
 
-													return (new);
+	return (new_node);
 }
