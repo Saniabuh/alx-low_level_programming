3e33aded5fbe613bb5ae07eee10aeac71a755e56
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-07-08 01:01:25 +0200

    Task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,21 +1,18 @@
 #include "lists.h"
 
 /**
- *  * get_dnodeint_at_index - Locates a node in a dlistint_t list.
- *   * @head: The head of the dlistint_t list.
- *    * @index: The node to locate.
- *     *
- *      * Return: If the node does not exist - NULL.
- *       *         Otherwise - the address of the located node.
- *        */
+ * get_dnodeint_at_index - return the nth node of a dlistint list
+ * @head: type pointer dlistint node
+ * @index: type unsigned int index position of the node
+ * Return: return head
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-		for (; index != 0; index--)
-				{
-							if (head == NULL)
-											return (NULL);
-									head = head->next;
-										}
-
-			return (head);
+	for (; index != 0; index--)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
 }
