f4d3b45b015e2a4ca287aad189f33401b4099981
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-07-08 09:58:38 +0100

    Task 12

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,12 +3,12 @@
 #include <string.h>
 
 /**
- *  main - Generates and prints passwords for the crackme5 executable.
- *  @argc: The number of arguments supplied to the program.
- *  @argv: An array of pointers to the arguments.
- *   
- *  Return: Always 0.
- **/
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	char password[7], *codex;
@@ -21,30 +21,30 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 
 	tmp = 0;
 	for (i = 0; i < len; i++)
-	tmp += argv[1][i];
+		tmp += argv[1][i];
 	password[1] = codex[(tmp ^ 79) & 63];
 
 	tmp = 1;
 	for (i = 0; i < len; i++)
-	tmp *= argv[1][i];
+		tmp *= argv[1][i];
 	password[2] = codex[(tmp ^ 85) & 63];
 
 	tmp = 0;
 	for (i = 0; i < len; i++)
- {
-	if (argv[1][i] > tmp)
-	tmp = argv[1][i];
- }
+	{
+		if (argv[1][i] > tmp)
+			tmp = argv[1][i];
+	}
 	srand(tmp ^ 14);
 	password[3] = codex[rand() & 63];
 
 	tmp = 0;
 	for (i = 0; i < len; i++)
-	tmp += (argv[1][i] * argv[1][i]);
+		tmp += (argv[1][i] * argv[1][i]);
 	password[4] = codex[(tmp ^ 239) & 63];
 
 	for (i = 0; i < argv[1][0]; i++)
-	tmp = rand();
+		tmp = rand();
 	password[5] = codex[(tmp ^ 229) & 63];
 
 	password[6] = '\0';
