84276a5cd95d050cb140db1a6db5968ce7c2c5b3
Saniabuh <Saniabuh@gmail.com>
2022-07-13 03:46:09 -0700

    Task

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+#include <stdio.h>                                                                                                                    
+                                                                                                                                
+int add(int a, int b){                                                                                                               
+	        int sum = a + b;                                                                                                              
+		        return sum;                                                                                                                   
+}                                                                                                                                     
+                                                                                                                                      
+int sub(int a, int b){                                                                                                               
+	        int difference = a - b;                                                                                                       
+		        return difference;                                                                                                            
+}                                                                                                                                     
+                                                                                                                                      
+int mul(int a, int b){                                                                                                               
+	        int multiply = a * b;                                                                                                         
+		        return multiply;                                                                                                              
+}                                                                                                                                     
+                                                                                                                                      
+int div(int a, int b){                                                                                                             
+	        float divide = a / b;                                                                                                         
+		        return divide;                                                                                                                
+}                                                                                                                                     
+                                                                                                                                      
+int mod(int a, int b){                                                                                                             
+	        int rem = a % b;                                                                                                              
+		        return rem;                                                                                                                   
+}                    
---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
